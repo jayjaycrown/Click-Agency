ef1a16bbb2760188ea66d2857aa2aca2e6e3cb2e
Ajayi Oluwafemi Adeyinka <ajayiadeyinka6991@gmail.com>
2020-02-23 15:46:03 +0100

    auth module desgin added

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,13 +4,28 @@ import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
-
+import { LoginComponent } from './login/login.component';
+import {RegisterComponent}  from  './register/register.component';
+import { ForgetComponent } from './forget/forget.component';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'dashboard',
+    redirectTo: 'login',
     pathMatch: 'full',
-  }, {
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'forget',
+    component: ForgetComponent
+  },
+  {
     path: '',
     component: AdminLayoutComponent,
     children: [{
