7fbf2110af8b1caeec4dae630688697ff8ddae58
jayjaycrown <olajidecrown@gmail.com>
2020-03-07 20:26:32 +0100

    l-u

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { LoginComponent } from './login/login.component';
-import {RegisterComponent}  from  './register/register.component';
+import {RegisterComponent} from './register/register.component';
 import { ForgetComponent } from './forget/forget.component';
 const routes: Routes = [
   {
